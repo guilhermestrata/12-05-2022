886a6fee501e426b7d8319114285dabe26159484
guilhermestrata <84384187+guilhermestrata@users.noreply.github.com>
2022-05-12 13:40:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//importar as configurações 
+const servidor = require("./config/servidor")
+//carregar o express 
+const app = servidor.app
+//carregar porta
+const porta = servidor.porta
+//carregar a rota index
+const index = require('./routes/index')(app)
+const login = require('./routes/login')(app)
+//ligar o servidor
+app.listen(porta, ()=>{
+    console.log('http://localhost:' + porta)
+})
